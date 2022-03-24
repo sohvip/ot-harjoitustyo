035471d19a9feaba269f1a79253a109c8cce879b
Sohvi Puska <sohvip1@gmail.com>
2022-03-24 20:22:52 +0200

    coverage

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+    
+    def test_lataa_rahaa_toimii(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.2")
+    
+    def test_saldo_vahenee_oikein(self):
+        self.maksukortti.ota_rahaa(5)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.05")
+    
+    def test_saldo_vahenee_oikein_ei_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(15)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def test_saldo_vahenee_oikein2(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(5), True)
+    
+    def test_saldo_vahenee_oikein_ei_tarpeeksi_rahaa2(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(15), False)

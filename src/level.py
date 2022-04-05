57f13485ecf22a6667d11ac54549faafd8a4b673
Sohvi Puska <sohvip1@gmail.com>
2022-04-05 11:18:46 +0300

    pelin rakenteen siistiminen

--- src/level.py
+++ src/level.py
@@ -1,72 +1,107 @@
 import pygame
 
-screen = pygame.display.set_mode((640,480))
-pygame.display.set_caption('Starkour')
+class Bg():
+    def __init__(self):
+        self.bg=pygame.image.load('assets/background.png')
+        self.x=0
 
-def level():
+class Starie():
+    def __init__(self):
+        self.starie=pygame.image.load('assets/starie.png')
+        self.starie=pygame.transform.rotozoom(self.starie,0,0.6)
+        self.y=318
+        self.jump=0
+        self.fall=1    
+        self.jumping=0
 
-    bg = pygame.image.load('assets/background.png')
-    bg_x = 0
+class Spike():
+    def __init__(self):
+        self.spike=pygame.image.load('assets/spike.png')
+        self.spike=pygame.transform.rotozoom(self.spike,0,0.7)
+        self.x=700
+        self.x2=1300
+        self.speed=1.3
 
-    starie = pygame.image.load('assets/starie.png')
-    starie = pygame.transform.rotozoom(starie,0,0.6)  
-    starie_y = 318
-    jump = 0
-    fall = 1
-    jumping = 0
+class Goal():
+    def __init__(self):
+        self.goal=pygame.image.load('assets/goal.png')
+        self.goal=pygame.transform.rotozoom(self.goal,0,1.1)
+        self.x=1700
+        self.speed=1.3
 
-    spike = pygame.image.load('assets/spike.png')
-    spike = pygame.transform.rotozoom(spike,0,0.7)
-    spike_x = 700
-    spike_speed = 1.3
+class Play():
+    def __init__(self):
+        pygame.init()
 
-    spike_2 = pygame.image.load('assets/spike.png')
-    spike_2 = pygame.transform.rotozoom(spike_2,0,0.7)
-    spike_x2 = 1300
+        self.screen=pygame.display.set_mode((640,480))
+        pygame.display.set_caption('Starkour')
 
-    goal = pygame.image.load('assets/goal.png')
-    goal = pygame.transform.rotozoom(goal,0,1.1)
-    goal_x = 1700
+        self.bg=Bg()
+        self.starie=Starie()
+        self.spike=Spike()
+        self.goal=Goal()
+        self.clock=pygame.time.Clock()
+        #self.fps=60
 
-    while True:
-        screen.blit(bg,(bg_x-640,0))
-        screen.blit(bg,(bg_x,0))
-        screen.blit(bg,(bg_x+640,0))
-        bg_x -= 0.5
-        if bg_x <= -640:
-            bg_x = 0
+        self.gameloop()
 
-        screen.blit(starie,(40,starie_y))
-        if starie_y < 318:
-            starie_y += fall
-        if jump == 1:
-            starie_y -= 4
-            jumping += 1
-            if jumping > 40:
-                jumping = 0
-                jump = 0
+    def gameloop(self):
+        while True:
+            #self.clock.tick(self.fps)
+            self.get_events()
+            self.draw()
 
-        screen.blit(spike,(spike_x,351))
-        spike_x -= spike_speed
-
-        spike_rect2 = screen.blit(spike_2,(spike_x2,351))
-        spike_x2 -= spike_speed
-
-        screen.blit(goal,(goal_x,270))
-        goal_x -= spike_speed
-        if goal_x < -25:
-            pygame.display.quit()
-            exit()
-
-        pygame.display.update()
+    def get_events(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type==pygame.QUIT:
                 pygame.display.quit()
                 exit()
-            if event.type == pygame.KEYDOWN:
+            if event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_SPACE:
-                    if starie_y == 318:
-                        jump = 1
+                    if self.starie.y==318:
+                        self.starie.jump=1
 
+    def draw(self):
+        self.draw_bg()
+        self.draw_starie()
+        self.draw_spikes()
+        self.draw_goal()
+
+        pygame.display.flip()
+
+    def draw_bg(self):
+        self.screen.blit(self.bg.bg,(self.bg.x-640,0))
+        self.screen.blit(self.bg.bg,(self.bg.x,0))
+        self.screen.blit(self.bg.bg,(self.bg.x+640,0))
+        self.bg.x-=0.5
+        if self.bg.x<=-640:
+            self.bg.x=0
+
+    def draw_starie(self):
+        self.screen.blit(self.starie.starie,(40,self.starie.y))
+        if self.starie.y<318:
+            self.starie.y+=self.starie.fall
+        if self.starie.jump==1:
+            self.starie.y-=4
+            self.starie.jumping+=1
+            if self.starie.jumping>40:
+                self.starie.jumping=0
+                self.starie.jump=0
+    
+    def draw_spikes(self):
+        self.screen.blit(self.spike.spike,(self.spike.x,351))
+        self.spike.x-=self.spike.speed
+
+        self.screen.blit(self.spike.spike,(self.spike.x2,351))
+        self.spike.x2-=self.spike.speed
+
+    def draw_goal(self):
+        self.screen.blit(self.goal.goal,(self.goal.x,270))
+        self.goal.x-=self.goal.speed
+        if self.goal.x<-25:
+            pygame.display.quit()
+            exit()
+        
 if __name__=="__main__":
-    level()
+    Play()
+    
--- src/tests/level_test.py
+++ src/tests/level_test.py
@@ -4,7 +4,3 @@ import level
 class TestLevel(unittest.TestCase):
     def setUp(self):
         return
-    
-    def test_level(self):
-        level=level()
-        self.assertTrue(level)

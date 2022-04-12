adcba1a52ad996005844c4fec6a0cbcff86807e9
Sohvi Puska <sohvip1@gmail.com>
2022-04-12 18:12:17 +0300

    uusi testi

--- src/tests/level_test.py
+++ src/tests/level_test.py
@@ -16,3 +16,7 @@ class TestLevel(unittest.TestCase):
         self.play.draw_spikes()
         self.assertEqual(self.play.score, 1)
     
+    def test_spike_move(self):
+        self.play.spike.x = -51
+        self.play.draw_spikes()
+        self.assertEqual(self.play.spike.x, 650)
